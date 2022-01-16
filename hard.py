4fbb230e1abe12b7368e88b19856f31d204d7073
z5360742 <z5360742@cse.unsw.edu.au>
2022-01-16 22:36:21 +1100

    Did some of hard, passed all autotests except 1, might not have enough time left to do the rest?

--- hard.py
+++ hard.py
@@ -35,7 +35,138 @@ def is_unlocked(courses_list, target_course):
     
     # TODO: COMPLETE THIS FUNCTION!!!
     
-    return True
+    validity = False
+
+    if target_course == "COMP1511": 
+        validity = True
+
+    if target_course == "COMP1521": 
+        for course in ["COMP1511", "DPST1091", "COMP1911", "COMP1917"]: 
+            if course in courses_list: 
+                validity = True
+    
+    if target_course == "COMP1531": 
+        for course in ["COMP1511", "DPST1091", "COMP1917", "COMP1921"]: 
+            if course in courses_list: 
+                validity = True
+    
+    if target_course == "COMP2041": 
+        for course in ["COMP1511", "DPST1091", "COMP1917", "COMP1921"]: 
+            if course in courses_list: 
+                validity = True
+
+    if target_course == "COMP2111": 
+        for course in ["COMP1511", "DPST1091", "COMP1917", "COMP1921"]: 
+            if course in courses_list: 
+                if "MATH1081" in courses_list: 
+                    validity = True
+
+    if target_course == "COMP2121": 
+        # COMP1917 OR COMP1921 OR COMP1511 OR DPST1091 OR COMP1521 OR DPST1092 OR (COMP1911 AND MTRN2500)
+        for course in ["COMP1917", "COMP1921", "COMP1511", "DPST1091", "COMP1521", "DPST1092"]: 
+            if course in courses_list: 
+                validity = True
+        if "COMP1911" in courses_list:
+            if "MTRN2500" in courses_list:
+                validity = True
+
+    #COMP2511": "COMP1531 AND (COMP2521 OR COMP1927)
+    if target_course == "COMP2511": 
+        for course in ["COMP2521", "COMP1927"]: 
+            if course in courses_list: 
+                if "COMP1531" in courses_list: 
+                    validity = True
+    
+    #COMP2521": "COMP1511    OR DPST1091 OR COMP1917 OR COMP1921
+    if target_course == "COMP2521": 
+        for course in ["COMP1511", "DPST1091", "COMP1917", "COMP1921"]:
+            if course in courses_list: 
+                validity = True
+    
+    #"COMP3121": "COMP1927 or    COMP2521."
+    if target_course == "COMP3121": 
+        for course in ["COMP1927", "COMP2521"]: 
+            if course in courses_list: 
+                validity = True
+
+    #"COMP3131": "COMP2511 or COMP2911"
+    if target_course == "COMP3131": 
+        for course in ["COMP2511", "COMP2911"]: 
+            if course in courses_list: 
+                validity = True
+
+    #"COMP3141": "COMP1927 or COMP2521."
+    if target_course == "COMP3141": 
+        for course in ["COMP1927", "COMP2521"]: 
+            if course in courses_list: 
+                validity = True
+
+    #"COMP3151": "COMP1927    OR ((COMP1521 or DPST1092) AND COMP2521)"
+    if target_course == "COMP3151": 
+        if "COMP1927" in courses_list: 
+            validity = True
+        elif "COMP2521" in courses_list: 
+            for course in ["COMP1521", "DPST1092"]: 
+                if course in courses_list:
+                    validity = True
+
+    #"COMP3153": "MATH1081",
+    if target_course == "COMP3153": 
+        if "MATH1081" in courses_list:
+            validity = True
+    
+    #"COMP3161": "COMP2521 or COMP1927"
+    if target_course == "COMP3161": 
+        for course in ["COMP2521", "COMP1927"]: 
+            if course in courses_list: 
+                validity = True
+    
+    #"COMP3211": "COMP3222 or ELEC2141",
+    if target_course == "COMP3211": 
+        for course in ["COMP3222", "ELEC2141"]: 
+            if course in courses_list: 
+                validity = True
+    
+    #"COMP3900": "COMP1531 and (COMP2521 or COMP1927) and 102 units of credit",
+    if target_course == "COMP3900": 
+        for course in ["COMP2521", "COMP1927"]: 
+            if course in courses_list: 
+                or_statement = True
+        if "COMP1531" and or_statement and len(courses_list) >= 17: 
+            validity = True
+    
+    #"COMP3901": "Prerequisite: 12 units of credit in  level 1 COMP courses and 18 units of credit in level 2 COMP courses"
+    if target_course == "COMP3901": 
+        COMP_1 = ["COMP1511", "COMP1521", "COMP1531"]
+        for i in range(2):
+            for course in COMP_1:
+                if (course in courses_list) and i == 0: 
+                    COMP_1.remove(course)
+                elif (course in courses_list) and i == 1:
+                    COMP_1_valid = True
+
+        COMP_2 = ["COMP2041", "COMP2111", "COMP2121", "COMP2511", "COMP2521"]
+        for i in range(3):
+            for course in COMP_2: 
+                if (course in courses_list) and i <= 1:
+                    COMP_2.remove(course)
+                elif (course in courses_list) and i == 2: 
+                    COMP_2_valid = True
+        
+        if COMP_1_valid and COMP_2_valid: 
+            validity = True
+
+    #"COMP4161": "Completion  of 18 units of credit"
+    if target_course == "COMP4161": 
+        if len(courses_list) >= 3:
+            validity = True
+
+    # I've done some of these to hopefully show my way of thinking
+    # Hoping I'll be able to get away with not doing the rest as they seem pretty 
+    # similar to most of the ones I've done
+
+
+    return validity
 
 
 
